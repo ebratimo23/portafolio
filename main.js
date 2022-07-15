89408905e5ce1ef330bea8668b8758e94503e63b
ebratimo <ebratimo23@gmail.com>
2022-07-15 01:00:27 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const iconMenu = document.querySelector("#icon-menu");
+const menu = document.querySelector("#menu");
+
+iconMenu.addEventListener('click', function (){
+    menu.classList.toggle("show_menu")
+})
